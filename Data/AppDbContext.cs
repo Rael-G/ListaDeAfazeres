b579016e24f4d5061503cbc7aeb9835d4cd0e627
Israel Albino Galvan <israel.galvan@live.com>
2023-04-25 20:24:44 -0300

    add models and context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ListaDeAfazeres.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaDeAfazeres.Data
+{
+	public class AppDbContext : DbContext 
+	{
+		public AppDbContext (DbContextOptions<AppDbContext> options) : base ( options) { }
+
+		DbSet<ToDo> ToDo { get; set; }
+
+		DbSet<Notes> Notes { get; set; }
+
+	}
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListaDeAfazeres.Models
+{
+	public class ToDo
+	{
+		[Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content{ get; set; }
+        [Required]
+        public bool IsComplete { get; set; }
+
+        public List<Notes> Notes { get; set; }
+
+
+    }
+}
